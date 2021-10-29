179efe670e3153e844ea13a3fdc445461e1ca366
twostepsahead <93393562+twostepsahead@users.noreply.github.com>
2021-10-29 11:42:20 -0400

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,13 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 14073
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.nio.file.Paths;
+
+
 public class FileReader {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Data data = new Data();
+        System.out.println("FileReader initialized.");
+        System.out.println("Options: read, help, exit");
+        
+        while (true) {
+            if (scanner.nextLine().equals("read")) {
+                System.out.println("Enter file name: ");
+                String fileName = scanner.nextLine();
+                
+                try (Scanner reader = new Scanner(Paths.get(fileName))) {
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        String[] parts = line.split(",");
+                        data.addEntry(parts[0], Integer.valueOf(parts[1]));
+                    }
+                    
+                    for (Data entry : data) {
+                        System.out.println(entry);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+                
+            } else if (scanner.nextLine().equals("help")) {
+                System.out.println("This program is designed to parse data files in csv format. The data should be in two columns with the name first and the data second");
+            } else if (scanner.nextLine().equals("exit")) {
+                System.out.println("Exit requested.");
+                break;
+            } else {
+                System.out.println("Invalid input!");
+                continue;
+            }
+        }   
+    }
+    
     
 }
