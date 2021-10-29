f5d105b88a45fe1c37d9350390234d522899962b
twostepsahead <93393562+twostepsahead@users.noreply.github.com>
2021-10-29 13:58:49 -0400

    Update Data.java

--- Data.java
+++ Data.java
@@ -3,17 +3,12 @@ import java.util.ArrayList;
 
 
 public class Data {
-    private ArrayList<String> name;
-    private ArrayList<Double> price;
+    private String name;
+    private double price;
     
-    public Data() {
-        this.name = new ArrayList<String>();
-        this.price = new ArrayList<Double>();
-    }
-    
-    public void addEntry(String name, double price) {
-        this.name.add(name);
-        this.price.add(price);
+    public Data(String name, double price) {
+        this.name = name;
+        this.price = price;
     }
     
     @Override
@@ -21,4 +16,3 @@ public class Data {
         return this.name + " " + this.price;
     }
 }
-
